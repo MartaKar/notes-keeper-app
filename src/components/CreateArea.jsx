9bd9c42d3e0bb8e5966a83ed233669173875369a
Marta <martakaras1@gmail.com>
2022-09-06 18:49:53 +0200

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+function CreateArea(props) {
+  const [inputText, setInputText] = useState({
+    title: "",
+    content: ""
+    });
+  function handleChange(event){
+    const {name, value} = event.target;
+    setInputText(prevValue => {
+      return {
+      ...prevValue,
+      [name]: value
+    }});    
+  }
+
+  function submitNote(event){
+    props.onAdd(inputText); 
+    setInputText({
+      title: "",
+      content: ""
+    });
+    event.preventDefault() 
+  }
+
+  return (
+    <div>
+      <form>
+        <input onChange={handleChange} name="title" placeholder="Title" value={inputText.title}/>
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={inputText.content} />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Header() {
+    return <header>
+    <h1 >Keeper app</h1>
+    </header>
+};
+export default Header
